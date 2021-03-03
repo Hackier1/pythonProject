e1b416460cab8c8df7f53ef6a13bd6628f0b36d5
Filip Klimek <73545976+Hackier1@users.noreply.github.com>
2021-03-03 17:23:05 +0100

    Add files via upload

---
+++ Zadania.py
@@ -0,0 +1,70 @@
+#Zad1
+a1='napis'
+a2='napis2'
+b1=5
+b2=8
+c1=2.3
+c2=4.5
+print(a1)
+print(a2)
+print(b1)
+print(b2)
+print(c1)
+print(c2)
+
+#Zad2
+kalkulator=2*2-2+2/2
+print(kalkulator)
+
+#Zad3
+a=5
+print(a)
+a+=2
+print(a)
+a-=4
+print(a)
+a*=3
+a/=1
+a**=4
+a%=2
+
+#Zad4
+import math
+a=math.e
+print (a**10)
+
+a=math.sin(8)*math.sin(8)
+a=math.log(5+a)
+a=a**1/6
+print (a)
+print(math.floor(3.55))
+print(math.ceil(4.80))
+
+#Zad5
+Imie='FILIP'
+print(Imie.capitalize())
+Nazwisko='KLIMEK'
+print(Nazwisko.capitalize())
+
+#Zad6
+piosenka="kocham cie la la la"
+la = piosenka.count("la")
+print(la)
+
+#Zad7
+lancuch="marcinowo"
+print(lancuch[1])
+print(lancuch[8])
+
+#Zad8
+podziel = piosenka.split(" ")
+print(podziel)
+
+#Zad9
+string="string"
+float=8.5
+hex=0xE
+
+print("drukuje %s, %2.1f, %X" % (string, float, hex))
+
+
